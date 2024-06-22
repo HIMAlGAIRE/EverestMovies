108175de37b67836bf857c2651c00a1f0360f18e
HIMAlGAIRE <119067369+HIMAlGAIRE@users.noreply.github.com>
2024-06-22 09:55:05 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById('menu-icon').addEventListener('click', function() {
     const navbar = document.getElementById('navbar');
-    if (navbar.style.display === 'flex' || navbar.style.display === '') {
+    if (navbar.style.display === 'flex') {
         navbar.style.display = 'none';
     } else {
         navbar.style.display = 'flex';
